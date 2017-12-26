f7deec4a9e8be2febba02a041b3ce63d81121205
GregorPiev <gregpiev@gmail.com>
2017-12-26 17:39:58 +0200

    26.12.2017-1

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
 ﻿var $ = require('jquery');
-require('bootstrap');
+window.jQuery = $;
+window.$ = $;
+require('bootstrap/dist/js/bootstrap.bundle.js');
+let Vue = require('vue/dist/vue.common.js');
+window.Vue = Vue;
 require('./app');
